dff6b42b44fec97eb701148d17898de344f8dffb
reese.bearden <reese.bearden@equipmentshare.com>
2023-04-12 10:31:26 -0500

    fixed scalability of video bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,24 @@ import '../App.css';
 import video from '../data/tiger.mp4'
 const Home = () => {
     return (
-        <div style={{width:'100%',height:'100%'}}>
-            <div style={{width:'100%',height:'100%'}}>
-                <video src={video} width='100%' autoPlay muted loop/>
-            </div>
+        <div style={{width:'100%',height:'100%', overflow: "hidden",
+        display: "flex",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: -1}}>
+            
+                <video src={video} style={{position: "absolute",
+    top: "50%",
+    left: "50%",
+    minWidth: "100%",
+    minHeight: "100%",
+    width: "auto",
+    height: "auto",
+    transform: "translate(-50%, -50%)"}} autoPlay muted loop/>
+            
         </div>
     );
 };
